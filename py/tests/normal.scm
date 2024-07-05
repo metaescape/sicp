643046433253e3eb1c0d8f6d2d52718bde11c17b
mE <metaescape@foxmail.com>
2024-07-05 22:45:09 +0800

    a normal-order scheme interpreter, tested with Y combinator

---
+++ py/tests/normal.scm
@@ -0,0 +1,27 @@
+#lang scheme
+
+(* 2 (* 2 4))
+
+((lambda (x) (* x x)) 3)
+
+((lambda (a b) (if (= 0 a) 1 b)) 0 (car '()))
+
+((lambda (f) (f 3)) (lambda (x) (* x x)))
+
+(((lambda (f)
+        ((lambda (x) (f (x x)))
+         (lambda (x) (f (x x)))))
+    (lambda (fact)
+        (lambda (n)
+            (if (<= n 1)
+                 1
+                (* n (fact (- n 1))))))) 4)
+
+(((lambda (f)
+        ((lambda (x) (f (x x)))
+         (lambda (x) (f (x x)))))
+    (lambda (fib)
+        (lambda (n)
+            (if (<= n 1)
+                 1
+                (+ (fib (- n 2)) (fib (- n 1))))))) 10)
