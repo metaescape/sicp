817cfecc0534ee3dc13f5ac5f9ca3bbf42981186
mE <metaescape@foxmail.com>
2024-07-06 20:34:56 +0800

    analyzer for calculator

---
+++ py/analysis0_calculator.py
@@ -0,0 +1,123 @@
+from typing import List
+import string
+import os
+
+# NUMERAL_STARTS is from https://www.composingprograms.com/examples/scalc/scheme_tokens.py.html
+_NUMERAL_STARTS = set(string.digits) | set("+-.")
+
+
+def tokenize(string):
+    return string.split()
+
+
+def clean_expression(expression: str):
+    return expression.replace("(", " ( ").replace(")", " ) ")
+
+
+def maybe_to_number(token: str):
+    if token in ["+", "-"] and len(token) == 1:
+        return token
+    if token[0] in _NUMERAL_STARTS:
+        try:
+            return int(token)
+        except ValueError:
+            try:
+                return float(token)
+            except ValueError:
+                raise ValueError("invalid numeral: {0}".format(token))
+    else:
+        return token
+
+
+def to_list_ast(expression: List) -> List:
+    """
+    ['(', '/', 4, '(',  '*', '1', '1', ')', ')'] -> [ '/', '4', ["*", 1, 1] ]
+    """
+    stack = []
+    for e in expression:
+        if e == ")":
+            temp = []
+            while stack[-1] != "(":
+                temp.append(stack.pop())
+            stack.pop()
+            stack.append(temp[::-1])
+        else:
+            stack.append(e)
+    return stack[0]
+
+
+def read_and_parse(path: str) -> List[List[str]]:
+    """
+    expressions are split by double "\n"
+    """
+    with open(path) as f:
+        string = f.read()
+
+    expressions_str = string.split("\n\n")
+
+    expressions = []
+    for exp in expressions_str:
+        if exp.startswith(";") or exp.startswith("#"):
+            continue
+        exp = clean_expression(exp)
+        tokens = tokenize(exp)
+        tokens = [maybe_to_number(token) for token in tokens]
+        exp = to_list_ast(tokens)
+        expressions.append(exp)
+    return expressions
+
+
+# codes above are for parsing the scheme code
+
+
+def evaluate(exp: list):
+    return analyze(exp)()
+
+
+def analyze(exp: list):
+    if isinstance(exp, (int, float)):
+        return lambda: exp
+    if type(exp) is str and exp in apply_map:
+        return lambda: apply_map[exp]
+    elif isinstance(exp, list):
+        proc = analyze(exp[0])
+        args = [analyze(arg) for arg in exp[1:]]
+        return lambda: proc()([arg() for arg in args])
+    else:
+        raise TypeError(f"{exp} is not a number, string, or list")
+
+
+apply_map = {
+    "+": lambda x: sum(x),
+    "-": lambda x: x[0] - x[1],
+    "*": lambda x: x[0] * x[1],
+    "/": lambda x: x[0] / x[1],
+    "//": lambda x: x[0] // x[1],
+}
+
+
+def evaluate_file(path: str):
+    expressions = read_and_parse(path)
+    for exp in expressions:
+        print(evaluate(exp))
+
+
+def test_evaluate_file():
+    path = os.path.abspath(__file__)
+    file_name = path.split("/")[-1].split(".")[0]
+    core = file_name.split("_")[-1]
+    test_file = f"tests/{core}.scm"
+    for exp in read_and_parse(test_file):
+        print(exp)
+    evaluate_file(test_file)
+
+
+if __name__ == "__main__":
+    # argparse for test
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--test", action="store_true")
+    args = parser.parse_args()
+    if args.test:
+        test_evaluate_file()
--- py/test.sh
+++ py/test.sh
@@ -1,30 +1,69 @@
 #!/bin/bash
 
-if [ "$1" = "calculator" ]; then
-    python eval0_calculator.py --test
-elif [ "$1" = "cons" ]; then
-    python eval1_cons.py --test
-elif [ "$1" = "cond" ]; then
-    python eval2_cond.py --test
-elif [ "$1" = "lambda" ]; then
-    python eval3_lambda.py --test
-elif [ "$1" = "closure" ]; then
-    python eval4_closure.py --test
-elif [ "$1" = "normal" ]; then
-    python eval5_normal.py --test
-elif [ "$1" = "let" ]; then
-    python eval6_let.py --test
-elif [ "$1" = "define" ]; then
-    python eval7_define.py --test
-elif [ "$1" = "all" ]; then
-    python eval0_calculator.py --test
-    python eval1_cons.py --test
-    python eval2_cond.py --test
-    python eval3_lambda.py --test
-    python eval4_closure.py --test
-    python eval5_normal.py --test
-    python eval6_let.py --test
-    python eval7_define.py --test
-else
-    echo "Invalid argument. Please specify a valid option."
+if [ "$1" = "analysis" ]; then
+    if [ "$2" = "calculator" ]; then
+        python analysis0_calculator.py --test
+    elif [ "$2" = "cons" ]; then
+        python analysis1_cons.py --test
+    elif [ "$2" = "cond" ]; then
+        python analysis2_cond.py --test
+    elif [ "$2" = "lambda" ]; then
+        python analysis3_lambda.py --test
+    elif [ "$2" = "closure" ]; then
+        python analysis4_closure.py --test
+    elif [ "$2" = "normal" ]; then
+        python analysis5_normal.py --test
+    elif [ "$2" = "let" ]; then
+        python analysis6_let.py --test
+    elif [ "$2" = "define" ]; then
+        python analysis7_define.py --test
+    elif [ "$2" = "mit" ]; then
+        python analysis8_mit.py --test
+    elif [ "$2" = "all" ]; then
+        python analysis0_calculator.py --test
+        python analysis1_cons.py --test
+        python analysis2_cond.py --test
+        python analysis3_lambda.py --test
+        python analysis4_closure.py --test
+        python analysis5_normal.py --test
+        python analysis6_let.py --test
+        python analysis7_define.py --test
+        python analysis8_mit.py --test
+    else
+        echo "Invalid argument. Please specify a valid option."
+    fi
+fi
+
+if [ "$1" = "eval" ]; then
+    if [ "$2" = "calculator" ]; then
+        python eval0_calculator.py --test
+    elif [ "$2" = "cons" ]; then
+        python eval1_cons.py --test
+    elif [ "$2" = "cond" ]; then
+        python eval2_cond.py --test
+    elif [ "$2" = "lambda" ]; then
+        python eval3_lambda.py --test
+    elif [ "$2" = "closure" ]; then
+        python eval4_closure.py --test
+    elif [ "$2" = "normal" ]; then
+        python eval5_normal.py --test
+    elif [ "$2" = "let" ]; then
+        python eval6_let.py --test
+    elif [ "$2" = "define" ]; then
+        python eval7_define.py --test
+    elif [ "$2" = "mit" ]; then
+        python eval8_mit.py --test
+    elif [ "$2" = "all" ]; then
+        python eval0_calculator.py --test
+        python eval1_cons.py --test
+        python eval2_cond.py --test
+        python eval3_lambda.py --test
+        python eval4_closure.py --test
+        python eval5_normal.py --test
+        python eval6_let.py --test
+        python eval7_define.py --test
+        python eval8_mit.py --test
+    else
+        echo "Invalid argument. Please specify a valid option."
+    fi
 fi
